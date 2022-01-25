d52e54f659c620479dd072dd17fd28fd3bd2b57b
pianokitkat <katelynnoleson@gmail.com>
2022-01-24 16:18:02 -0800

    yubaba bounding box

--- GameEngine/yubaba.js
+++ GameEngine/yubaba.js
@@ -1,50 +1,49 @@
-// for all new JS files, must load them into index.html!
-// all sprites/entities have update and draw
-
 class Yubaba {
-    constructor(game){
+    constructor(game, x, y){
         //screen size stuff
         /* width="400" height="400" */
-        const SCALE = 0.35; // how do these work?
-        this.scale = 0.3;
 
         // sprite stuff
+        Object.assign(this, { game, x, y});
         this.path = ASSET_MANAGER.getAsset("./sprites/yubaba.png");
-        this.x = 0
-        this.y = 0; 
         this.width = 278;
         this.height = 230;
         this.frameCount = 14;
         this.frameDuration = 0.15; 
-        /* right = 0, left = 1*/
+        this.scale = 0.3; 
+
+        this.loadAnimations();
+
+        //bounding box
+        this.BB = new BoundingBox(this.x+this.width*3/8 *this.scale, this.y+this.height*1/8*this.scale, this.width*3/8*this.scale, this.height*3/4*this.scale);
+        
+        // speed stuff
+        this.speed = 50;
+
+    };
+
+    loadAnimations(){
+         /* right = 0, left = 1*/
         this.animations = [];
         this.animations[0] = new Animator(this.path, this.x, this.y, this.width, 
                 this.height, this.frameCount, this.frameDuration, false);
         this.animations[1] = new Animator(this.path, this.x, this.y+this.height, this.width, 
                 this.height, this.frameCount, this.frameDuration, false);
         this.animator = this.animations[0];
-        this.game = game;
-
-        //bounding box
-        this.BB = new BoundingBox(this.x+this.width*3/8, this.y+this.height*1/8, this.width*3/8, this.height*3/4);
-
-        // speed stuff
-        this.speed = 50;
-
-    };
+    }
 
     update(){
-       this.x += this.speed * this.game.clockTick;
-
-       this.BB = new BoundingBox(this.x+this.width*3/8, this.y+this.height*1/8, this.width*3/8, this.height*3/4);
-
-       if(this.x + this.width /* *this.scale */ >= PARAMS.CANVAS_WIDTH){ 
+        if(this.x + this.width *this.scale >= PARAMS.CANVAS_WIDTH){ 
             this.speed = -Math.abs(this.speed);
             this.animator = this.animations[1];
        }else if(this.x <= 0){
             this.speed = Math.abs(this.speed);   
             this.animator = this.animations[0];       
        }
+
+        this.x += this.speed * this.game.clockTick;
+        this.BB = new BoundingBox(this.x+this.width*3/8 *this.scale, this.y+this.height*1/8*this.scale, this.width*3/8*this.scale, this.height*3/4*this.scale);
+
     };
 
     /*
@@ -53,9 +52,9 @@ class Yubaba {
     draw(ctx){ 
         this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
 
-        // change to only display on debug
-        if(true){
-           // this.animator.drawBoundingBox(ctx, this.BB);
-        } 
+        if (PARAMS.DEBUG) {
+            ctx.strokeStyle = 'Red';
+            ctx.strokeRect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
+        }
     };
 }
