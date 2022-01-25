2e4528a04957d8acc1c3e947f75d4921db406082
pianokitkat <katelynnoleson@gmail.com>
2022-01-24 16:32:39 -0800

    yubaba working

--- GameEngine/yubaba.js
+++ GameEngine/yubaba.js
@@ -16,7 +16,7 @@ class Yubaba {
 
         //bounding box
         this.BB = new BoundingBox(this.x+this.width*3/8 *this.scale, this.y+this.height*1/8*this.scale, this.width*3/8*this.scale, this.height*3/4*this.scale);
-        
+
         // speed stuff
         this.speed = 50;
 
@@ -24,11 +24,12 @@ class Yubaba {
 
     loadAnimations(){
          /* right = 0, left = 1*/
+        this.dir = 0;
         this.animations = [];
         this.animations[0] = new Animator(this.path, this.x, this.y, this.width, 
-                this.height, this.frameCount, this.frameDuration, false);
+                this.height, this.frameCount, this.frameDuration, 0, false, true);
         this.animations[1] = new Animator(this.path, this.x, this.y+this.height, this.width, 
-                this.height, this.frameCount, this.frameDuration, false);
+                this.height, this.frameCount, this.frameDuration, 0, false, true);
         this.animator = this.animations[0];
     }
 
@@ -36,14 +37,22 @@ class Yubaba {
         if(this.x + this.width *this.scale >= PARAMS.CANVAS_WIDTH){ 
             this.speed = -Math.abs(this.speed);
             this.animator = this.animations[1];
+            this.dir = 1;
        }else if(this.x <= 0){
             this.speed = Math.abs(this.speed);   
-            this.animator = this.animations[0];       
+            this.animator = this.animations[0];  
+            this.dir = 0;     
        }
 
         this.x += this.speed * this.game.clockTick;
+       if(this.dir == 1){
+        // adjust bounding box for reverse direction? 
+        this.BB = new BoundingBox(this.x+this.width*3/8*this.scale, this.y+this.height*1/8*this.scale, this.width*3/8*this.scale, this.height*3/4*this.scale);
+
+       }else{
         this.BB = new BoundingBox(this.x+this.width*3/8 *this.scale, this.y+this.height*1/8*this.scale, this.width*3/8*this.scale, this.height*3/4*this.scale);
 
+       }
     };
 
     /*
