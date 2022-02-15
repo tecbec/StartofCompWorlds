13abb8d7fe448e147976b1931575a451fa2f3f5c
KDunn <ktboike@uw.edu>
2022-02-14 16:53:21 -0800

    merge with chau and change player position

--- GameEngine/player.js
+++ GameEngine/player.js
@@ -1,7 +1,7 @@
 /* Chihiro's Params */
 var CHIHIRO = {
     TITLE_POSITION:   {X: 0,  Y: 800},
-    INITIAL_POSITION: {X: 14739,  Y: 0},  
+    INITIAL_POSITION: {X: -200,  Y: 0},  // 14739
     SIZE: 70,
     SCALE: 2,
     PADDING:{X: 28, Y: 20}, // same padding for BB and imaginary x,y,w,h calculations
