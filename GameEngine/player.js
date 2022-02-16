c33f8067dd5b0fa2c2cd561cb6faaa941c2a8423
KDunn <ktboike@uw.edu>
2022-02-16 14:34:40 -0800

    formatting

--- GameEngine/player.js
+++ GameEngine/player.js
@@ -270,8 +270,7 @@ class Player {
             if (entity.BB && that.BB.collide(entity.BB)) {      // is there an entity bb & check to see if they collide
 
                 if (that.velocity.y > 0) {                      // chihiro is falling
-                    if((entity instanceof Ground || entity instanceof Platform || entity instanceof CloudPlatform ||
-                        entity instanceof StoneLamp ||
+                    if((entity instanceof Ground || entity instanceof Platform || entity instanceof CloudPlatform || entity instanceof StoneLamp ||
                         entity instanceof Railing || entity instanceof Lamp) && (that.lastBB.bottom  <= entity.BB.top)) // minus one?? idk how this works
                   { // bottom of chihiro hits the top of the entity
                         that.isGrounded = true;
