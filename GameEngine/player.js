ae1d62912f5c4c1edf476dd1fa4e38e62d195f25
Hana Bechara <hanabechara@Hanas-MacBook-Pro.local>
2022-02-20 14:21:31 -0800

    initial position should be 0

--- GameEngine/player.js
+++ GameEngine/player.js
@@ -1,7 +1,7 @@
 /* Chihiro's Params */
 var CHIHIRO = {
     TITLE_POSITION:   {X: 0,  Y: 800},
-    INITIAL_POSITION: {X: -200,  Y: 0},  // change to 10200 to test winning condition. 
+    INITIAL_POSITION: {X: 0,  Y: 0},  // change to 10200 to test winning condition. 
     SIZE: 70,
     SCALE: 2,
     PADDING:{X: 28, Y: 20}, // same padding for BB and imaginary x,y,w,h calculations
